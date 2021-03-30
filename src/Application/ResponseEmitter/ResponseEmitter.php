98e0ca67e5f64695e5b4bce0852bf1640f5513c1
rnavia <roger.navia.g@gmail.com>
2021-03-30 09:46:17 -0400

    header respuesta

--- src/Application/ResponseEmitter/ResponseEmitter.php
+++ src/Application/ResponseEmitter/ResponseEmitter.php
@@ -17,16 +17,14 @@ class ResponseEmitter extends SlimResponseEmitter
         $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
 
         $response = $response
-            //->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
             //->withHeader('Access-Control-Allow-Origin', $origin)
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, X-Auth-Token')
-                
-                
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-            //->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-            //->withAddedHeader('Cache-Control', 'post-check=0, pre-check=0')
-            //->withHeader('Pragma', 'no-cache');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+            ->withAddedHeader('Cache-Control', 'post-check=0, pre-check=0')
+            ->withHeader('Pragma', 'no-cache');
         if (ob_get_contents()) {
             ob_clean();
         }
