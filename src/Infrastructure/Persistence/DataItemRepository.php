f4610dd850201254fe366ff42a8a9fcefa523c17
JuKa <jucaco89@gmail.com>
2021-05-13 12:06:38 -0400

    fix list item

--- src/Infrastructure/Persistence/DataItemRepository.php
+++ src/Infrastructure/Persistence/DataItemRepository.php
@@ -152,7 +152,8 @@ class DataItemRepository implements ItemRepository {
             $concat=array('resultados'=>$arrayres,'total'=>$total);
             $resp = array('success'=>true,'message'=>'Exito','data_item'=>$concat);
         }else{
-            $resp = array('success'=>false,'message'=>'No se encontraron registros');
+            $concat=array('resultados'=>array(),'total'=>0);
+            $resp = array('success'=>true,'message'=>'No se encontraron registros','data_item'=>$concat);
         }
         return $resp;
     }
